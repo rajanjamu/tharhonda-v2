8b1c877304b98753a5089dfb13146f1d55e29752
Rajan Gupta <rajanjamu@gmail.com>
2015-06-01 01:36:42 +0530

    seventh commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :products
+  get "about" => "home#about"
+  get "contact" => "home#contact"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
